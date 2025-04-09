a756eea8786e71debc1f3ab770861b224a7d080c
Day HONG <tot032874@gmail.com>
2025-04-09 23:38:01 +0900

    TimSort refactoring

--- algorithms/TimSort.java
+++ algorithms/TimSort.java
@@ -9,212 +9,196 @@ public class TimSort extends Sortings {
     int stacki;
 
     @Override
-    public Item[] getsorted(Item[] tobesorted){
+    public Item[] getsorted(Item[] tobesorted) {
+        // minrun 계산 (Java 표준 기반)
+        int n = tobesorted.length;
+        int r = 0;
+        while (n >= 64) {
+            r |= (n & 1);
+            n >>= 1;
+        }
+        this.minrun = n + r;
 
-        // run의 개수 = n / minrun을 2^k로 만드는, 즉 minrun = n / 2^k (32<=minrun<64)
-        this.minrun = tobesorted.length;
-        while (minrun >= 64) minrun /= 2;
-        if (minrun < 32) minrun = 32;
+        this.runstack = new Run[(tobesorted.length / minrun) + 1];
+        this.stacki = 0;
 
-        this.runstack = new Run[(int)(tobesorted.length/minrun)+1];
+        // run들을 스택에 쌓음
         pushruns(tobesorted);
 
-        // final merge
-        while (this.stacki > 1) partial_merge(tobesorted, stacki-2);
-
+        // run 병합 (stack이 하나가 될 때까지)
+        while (this.stacki > 1) {
+            int before = this.stacki;
+            invariant(tobesorted);
+            if (this.stacki == before) {
+                // 조건에 맞는 병합이 일어나지 않았다면 강제로 병합
+                partial_merge(tobesorted, stacki - 2);
+            }
+        }
         return tobesorted;
     }
 
-    private void invariant(Item[] tobesorted){
-        while (this.stacki > 1){
-            int top = this.stacki-1;
-            if (top >= 2){
-                Run X = this.runstack[top-2];
-                Run Y = this.runstack[top-1];
+    private void invariant(Item[] tobesorted) {
+        while (this.stacki > 1) {
+            int top = this.stacki - 1;
+            if (top >= 2) {
+                Run X = this.runstack[top - 2];
+                Run Y = this.runstack[top - 1];
                 Run Z = this.runstack[top];
-
-                if (Z.length <= X.length + Y.length || X.length >= Y.length){
-                    if (X.length < Z.length) partial_merge(tobesorted, top-2);
-                    else partial_merge(tobesorted, top-1);
+                if (Z.length <= X.length + Y.length || X.length >= Y.length) {
+                    if (X.length < Z.length)
+                        partial_merge(tobesorted, top - 2);
+                    else
+                        partial_merge(tobesorted, top - 1);
                 } else break;
-
-            }
-            else if (top == 1){
-                Run Y = this.runstack[top-1];
+            } else if (top == 1) {
+                Run Y = this.runstack[top - 1];
                 Run Z = this.runstack[top];
-
-                if (Z.length <= Y.length) partial_merge(tobesorted, top-1);
+                if (Z.length <= Y.length)
+                    partial_merge(tobesorted, top - 1);
                 else break;
-            }
-            else break;
+            } else break;
         }
     }
 
-    private void partial_merge(Item[] tobesorted, int top){
-        // top 이랑 top+1이랑 merge하고 top 자리에 삽입한 후, top+1쪽으로 뒤에있는것들 땡겨오고
-        Run A = this.runstack [top];
-        Run B = this.runstack [top+1];
+    private void partial_merge(Item[] tobesorted, int top) {
+        Run A = this.runstack[top];
+        Run B = this.runstack[top + 1];
         Item[] merged = new Item[A.length + B.length];
 
         int i = 0, j = 0, k = 0;
         int countA = 0, countB = 0;
-        while (i < A.length || j < B.length){
+        while (i < A.length || j < B.length) {
             if (i == A.length) {
-                merged[k++] = tobesorted[B.start+j];
-                j++;
-                countB ++;
+                merged[k++] = tobesorted[B.start + j++];
+                countB++;
                 countA = 0;
-            }
-            else if (j == B.length) {
-                merged[k++] = tobesorted[A.start+i];
-                i++;
-                countA ++;
+            } else if (j == B.length) {
+                merged[k++] = tobesorted[A.start + i++];
+                countA++;
                 countB = 0;
-            }
-            else if (tobesorted[A.start+i].value > tobesorted[B.start+j].value) {
-                merged[k++] = tobesorted[B.start+j];
-                j++;
-                countB ++;
+            } else if (tobesorted[A.start + i].value <= tobesorted[B.start + j].value) {
+                merged[k++] = tobesorted[A.start + i++];
+                countA++;
+                countB = 0;
+            } else {
+                merged[k++] = tobesorted[B.start + j++];
+                countB++;
                 countA = 0;
-            }
-            else if (tobesorted[A.start+i].value <= tobesorted[B.start+j].value) {
-                merged[k++] = tobesorted[A.start+i];
-                i++;
-                countA ++;
-                countB = 0;
-            }
-            // galloping
-            if (countA >= 7){
-                int gal = gallop(tobesorted, tobesorted[B.start+j].value, A, i);
-                gal = Math.min(gal, A.length - i);
-                for (int g = 0; g < gal; g++){
-                    merged[k+g] = tobesorted[A.start+i+g];
-                }
-                i += gal;
-                k += gal;
-                countA = 0;
-                countB = 0;
-            }
-            else if (countB >= 7){
-                int gal = gallop(tobesorted, tobesorted[A.start+i].value, B, j);
-                gal = Math.min(gal, B.length - j);
-                for (int g = 0; g < gal; g++){
-                    merged[k+g] = tobesorted[B.start+j+g];
-                }
-                j += gal;
-                k += gal;
-                countA = 0;
-                countB = 0;
             }
 
+            // Galloping
+            if (countA >= 7 && j < B.length) {
+                int gal = gallop(tobesorted, tobesorted[B.start + j].value, A, i);
+                gal = Math.min(gal, A.length - i);
+                if (gal > 0) {
+                    System.arraycopy(tobesorted, A.start + i, merged, k, gal);
+                    i += gal;
+                    k += gal;
+                    countA = 0;
+                    countB = 0;
+                }
+            } else if (countB >= 7 && i < A.length) {
+                int gal = gallop(tobesorted, tobesorted[A.start + i].value, B, j);
+                gal = Math.min(gal, B.length - j);
+                if (gal > 0) {
+                    System.arraycopy(tobesorted, B.start + j, merged, k, gal);
+                    j += gal;
+                    k += gal;
+                    countA = 0;
+                    countB = 0;
+                }
+            }
         }
-        for (int part = 0; part < A.length+B.length; part++){
-            tobesorted[A.start+part] = merged[part];
-        }
-        this.runstack [top] = new Run(A.start, A.length+B.length);
+
+        System.arraycopy(merged, 0, tobesorted, A.start, A.length + B.length);
+        this.runstack[top] = new Run(A.start, A.length + B.length);
+
+        // stack 조정
         for (int s = top + 1; s < this.stacki - 1; s++) {
             this.runstack[s] = this.runstack[s + 1];
         }
         this.runstack[--this.stacki] = null;
     }
 
-    private int gallop(Item[] tobesorted, int target, Run X, int start){
+    private int gallop(Item[] tobesorted, int target, Run X, int start) {
         int offset = 1;
         int idx = start;
 
-        while (idx + offset < X.length && tobesorted[X.start+idx+offset].value < target) offset *= 2;
+        while (idx + offset < X.length && tobesorted[X.start + idx + offset].value < target)
+            offset *= 2;
 
         int low = idx + (offset / 2);
-        int high = Math.min(idx+offset, X.length);
-        while (low < high){
-            int mid = (low+high)/2;
-            if (tobesorted[X.start+mid].value < target) low = mid + 1;
-            else high = mid;
+        int high = Math.min(idx + offset, X.length);
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (tobesorted[X.start + mid].value < target)
+                low = mid + 1;
+            else
+                high = mid;
         }
-        return low-start;
+        return low - start;
     }
 
-    private void pushruns(Item[] tobesorted){
-        int runstart;
+    private void pushruns(Item[] tobesorted) {
         int i = 0;
-        this.stacki = 0;
-        while (i < tobesorted.length - 1){
+        while (i < tobesorted.length) {
+            int runstart = i;
             int runlen = 1;
-            boolean asc = (tobesorted[i].value <= tobesorted[i+1].value);
-    
-            if (asc){
-                runstart = i;
-                do {
-                    i++;
-                    runlen++;
-                } while(i < tobesorted.length-1 && tobesorted[i].value <= tobesorted[i+1].value);
+
+            // strictly increasing or decreasing
+            boolean asc = (i + 1 < tobesorted.length) && (tobesorted[i].value <= tobesorted[i + 1].value);
+            while (i + 1 < tobesorted.length &&
+                   (asc ? tobesorted[i].value <= tobesorted[i + 1].value
+                        : tobesorted[i].value > tobesorted[i + 1].value)) {
+                i++;
+                runlen++;
             }
-            else {
-                runstart = i;
-                do {
-                    i++;
-                    runlen++;
-                } while(i < tobesorted.length-1 && tobesorted[i].value > tobesorted[i+1].value);
-            }
-            
-            // reverse
+
+            // reverse if descending
             if (!asc) {
-                int l = runstart;
-                int r = runstart + runlen - 1;
-                while (l < r){
+                int l = runstart, r = runstart + runlen - 1;
+                while (l < r) {
                     Item temp = tobesorted[l];
-                    tobesorted[l] = tobesorted[r];
-                    tobesorted[r] = temp;
-                    l++;
-                    r--;
+                    tobesorted[l++] = tobesorted[r];
+                    tobesorted[r--] = temp;
                 }
             }
 
-            if (runlen < this.minrun){
-                int end = Math.min(runstart+this.minrun, tobesorted.length);
+            // pad to minrun
+            int end = runstart + runlen;
+            if (runlen < this.minrun) {
+                end = Math.min(runstart + this.minrun, tobesorted.length);
                 insertionRun(tobesorted, runstart, end);
-                int newlen = end-runstart;
-                this.runstack[this.stacki] = new Run(runstart,newlen);
-                i = end;
-            }
-            else {
-                this.runstack[this.stacki] = new Run(runstart,runlen);
+                runlen = end - runstart;
+            } else {
                 i++;
             }
-            this.stacki++;
-            // invariant
-            invariant(tobesorted);
-        }
-        if (i==tobesorted.length-1) {
-            this.runstack[this.stacki] = new Run(i, 1);
-            this.stacki++;
-            // invariant
+
+            this.runstack[this.stacki++] = new Run(runstart, runlen);
+            i = end;
             invariant(tobesorted);
         }
     }
 
-    private void insertionRun(Item[] tobesorted, int runstart, int end){
-        for (int i = runstart; i < end; i++) {
+    private void insertionRun(Item[] tobesorted, int runstart, int end) {
+        for (int i = runstart + 1; i < end; i++) {
             Item target = tobesorted[i];
-            int j = i-1;
-            while(j>=runstart){
-                if(tobesorted[j].value>target.value){
-                    tobesorted[j+1] = tobesorted[j];
-                    j--;
-                }
-                else break;
+            int j = i - 1;
+            while (j >= runstart && tobesorted[j].value > target.value) {
+                tobesorted[j + 1] = tobesorted[j];
+                j--;
             }
-            tobesorted[j+1] = target;
+            tobesorted[j + 1] = target;
         }
     }
-    
 }
 
-class Run{
+class Run {
     int start;
     int length;
 
-    Run(int start, int length){
+    Run(int start, int length) {
         this.start = start;
         this.length = length;
     }
